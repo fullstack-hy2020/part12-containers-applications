aefbf315177af7cc4d08e385cc2f44d32b58d61c
Matti Luukkainen <mluukkai@iki.fi>
2023-02-20 23:58:35 +0200

    tweak

--- todo-app/todo-frontend/src/index.js
+++ todo-app/todo-frontend/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 );
-
