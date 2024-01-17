23b6628e3933963a922995126c1620bc6eb91b16
helmer <helmer.lauronen@gmail.com>
2024-01-17 15:58:45 +0200

    12.11

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Todo } = require("../mongo");
 const router = express.Router();
+const { getAsync, setAsync } = require("../redis/index");
 
 /* GET todos listing. */
 router.get("/", async (_, res) => {
@@ -14,6 +15,8 @@ router.post("/", async (req, res) => {
 		text: req.body.text,
 		done: false,
 	});
+	const amt = Number(await getAsync("added_todos"));
+	await setAsync("added_todos", amt + 1);
 	res.send(todo);
 });
 
