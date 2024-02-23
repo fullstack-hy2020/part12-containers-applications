10d8dcf1ba8436834ad5dfbe3f6558d8d1c53aa5
helmer <helmer.lauronen@gmail.com>
2024-02-23 18:38:02 +0200

    Fix tabs

--- todo-app/todo-frontend/src/Todos/List.js
+++ todo-app/todo-frontend/src/Todos/List.js
@@ -34,15 +34,15 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
         )
 
         return (
-					<div key={todo._id}>
-						<hr />
-						<div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-							<span>
-								{todo.text} 
-							</span>
-							{todo.done ? doneInfo : notDoneInfo}
-						</div>
-					</div>
+          <div key={todo._id}>
+            <hr />
+            <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
+              <span>
+                {todo.text}
+              </span>
+              {todo.done ? doneInfo : notDoneInfo}
+            </div>
+          </div>
         )
       })}
     </>
